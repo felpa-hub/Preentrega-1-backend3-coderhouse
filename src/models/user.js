2f36f14f1b9e617c32af6e302c5ca008b0d6124e
felpa-hub <68981238+felpa-hub@users.noreply.github.com>
2025-03-04 17:52:55 -0300

    Create user.js

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  pets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pet' }],
+});
+
+// Middleware para encriptar la contraseña antes de guardar el usuario
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
