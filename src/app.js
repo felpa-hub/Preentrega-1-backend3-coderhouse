c06939bf00ee716db9075be32edb01ac63ad6c22
felpa-hub <68981238+felpa-hub@users.noreply.github.com>
2025-03-04 17:52:15 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const { PORT } = require('../config/config');
 const mocksRouter = require('./routes/mocks.router');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { MONGODB_URI, PORT } = process.env;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Conexión a MongoDB establecida'))
+.catch((error) => console.error('Error al conectar a MongoDB:', error));
+
 
 app.use(express.json()); // Middleware para parsear JSON
 
