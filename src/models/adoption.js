b56133a874f332589fa61b3a1391854c11160ecd
felpa-hub <68981238+felpa-hub@users.noreply.github.com>
2025-03-18 13:56:06 -0300

    Update adoption.js

--- src/models/adoption.js
+++ src/models/adoption.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const adoptionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   pet: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet', required: true },
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now }
 });
 
-const Adoption = mongoose.model('Adoption', adoptionSchema);
-
-module.exports = Adoption;
+module.exports = mongoose.model('Adoption', adoptionSchema);
